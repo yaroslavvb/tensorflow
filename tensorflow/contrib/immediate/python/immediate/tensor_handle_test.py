d972f4f37b99daa7f8877b1814a62a29711c259b
Yaroslav Bulatov <yaroslavvb@gmail.com>
2016-05-31 13:04:06 -0700

    Tests showing tensor_handle placement errors.

--- tensorflow/contrib/immediate/python/immediate/tensor_handle_test.py
+++ tensorflow/contrib/immediate/python/immediate/tensor_handle_test.py
@@ -7,6 +7,46 @@ from tensorflow.contrib.immediate.python.immediate import test_util
 
 class EnvTest(tf.test.TestCase):
 
+  def testPlaceholderOnGpuIssue(self):
+    # https://github.com/tensorflow/tensorflow/issues/2587
+    config = tf.ConfigProto(log_device_placement=True)
+    with self.test_session(config=config) as sess:
+      dtype=tf.float32
+      with tf.device("/gpu:0"):
+        a = tf.constant(1, dtype)
+
+      a_handle = sess.run(tf.get_session_handle(a))
+      b_holder, b_tensor = tf.get_session_tensor(dtype)
+      print(sess.run(b_tensor, feed_dict={b_holder:
+                                          a_handle.handle}))
+
+  def testSessionTensorOnGpuIssue(self):
+    # https://github.com/tensorflow/tensorflow/issues/2586
+    with self.test_session() as sess:                                           
+      a = tf.constant(1.0)                                                      
+      a_handle_op = tf.get_session_handle(a)                                    
+      b = tf.constant(2.0)                                                      
+      b_handle_op = tf.get_session_handle(b)                                    
+
+      failure_case = True                                                       
+      if failure_case:                                                          
+        a_p, a_t = tf.get_session_tensor(tf.float32)                            
+        b_p, b_t = tf.get_session_tensor(tf.float32)                            
+        a_handle = sess.run(a_handle_op)                                        
+        b_handle = sess.run(b_handle_op)                                        
+      else:                                                                     
+        a_handle = sess.run(a_handle_op)                                        
+        b_handle = sess.run(b_handle_op)                                        
+        a_p, a_t = tf.get_session_tensor(tf.float32)                            
+        b_p, b_t = tf.get_session_tensor(tf.float32)                            
+
+      c = tf.add(a_t, b_t)                                                      
+      c_handle = sess.run(                                                      
+        tf.get_session_handle(c),                                               
+        feed_dict={a_p: a_handle.handle,                                        
+                   b_p: b_handle.handle})                                       
+      self.assertEqual(3.0, c_handle.eval())                                    
+
   def testHandle(self):
     def testHandleForType(tf_dtype):
       for use_gpu in [True, False]:
