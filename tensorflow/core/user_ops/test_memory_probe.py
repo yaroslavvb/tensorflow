662f2b435bd02058791f08df9ea16a959062d645
Yaroslav Bulatov <yaroslavvb@gmail.com>
2017-01-25 19:24:40 -0800

    memory probe ops

--- tensorflow/core/user_ops/test_memory_probe.py
+++ tensorflow/core/user_ops/test_memory_probe.py
@@ -1,10 +1,25 @@
+# To use, make sure memory_probe_ops.so is in $PATH or current directory
+
 import tensorflow as tf
 import numpy as np
 import os
-home = os.environ['HOME']
+import sys
 
+so_name = "memory_probe_ops.so"
+
+paths_to_search = os.environ['PATH'].split(':')
+paths_to_search.append('.')
+for path in paths_to_search:
+    full_so_name = os.path.join(path, so_name)
+    if os.path.exists(full_so_name):
+        print("Loading %s" %(full_so_name))
+        memory_probe_ops = tf.load_op_library(full_so_name)
+        break
+else:
+    print("Unable to find %s in . or $PATH, quitting"%(so_name))
+    sys.exit()
+    
 run_with_tracing = False
-custom_module = tf.load_op_library("/Users/yaroslav/tensorflow.git/tensorflow/bazel-bin/tensorflow/core/user_ops/memory_probe_ops.so")
 config = tf.ConfigProto(graph_options=tf.GraphOptions(optimizer_options=tf.OptimizerOptions(opt_level=tf.OptimizerOptions.L0)))
 
 for d in ["/cpu:0", "/gpu:0"]:
@@ -15,12 +30,12 @@ for d in ["/cpu:0", "/gpu:0"]:
         inputs = tf.random_uniform((n,))
         print("Allocating %d MB variable on %s"%(mbs, d,))
         var = tf.Variable(inputs)
-        probe_op = custom_module.bytes_in_use()
-        max_op = custom_module.bytes_limit()
+        probe_op = memory_probe_ops.bytes_in_use()
+        max_op = memory_probe_ops.bytes_limit()
         run_metadata = tf.RunMetadata()
         run_options = tf.RunOptions(trace_level=tf.RunOptions.FULL_TRACE)
-        print("Before init %d out of %d bytes" % tuple(sess.run([probe_op, max_op])))
+        print("Before init %10d out of %10d bytes" % tuple(sess.run([probe_op, max_op])))
         sess.run(var.initializer)
-        print("After init %d out of %d bytes" % tuple(sess.run([probe_op, max_op])))
+        print("After  init %10d out of %10d bytes" % tuple(sess.run([probe_op, max_op])))
         if run_with_tracing:
             print(run_metadata)
